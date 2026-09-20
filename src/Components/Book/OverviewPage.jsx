03be241103215a204d730b1710d7041594e16eba
Ben Brown <bentombrown@gmail.com>
2026-09-20 15:38:12 -0500

    fixing overflow errors

--- src/Components/Book/OverviewPage.jsx
+++ src/Components/Book/OverviewPage.jsx
@@ -8,12 +8,15 @@ const OverviewPage = React.forwardRef(({ project, page, onExpand }, ref) => (
     <div className="page-content project">
       <h2>{project.title}</h2>
       {page.role && <p className="project-role">{page.role}</p>}
-      {page.image && (
-        <figure className="content-image">
-          <ExpandableImage src={page.image} alt={project.title} onExpand={onExpand} />
-        </figure>
-      )}
-      <p className="project-description">{page.description}</p>
+
+      <div className="page-scroll">
+        {page.image && (
+          <figure className="content-image">
+            <ExpandableImage src={page.image} alt={project.title} onExpand={onExpand} />
+          </figure>
+        )}
+        <p className="project-description">{page.description}</p>
+      </div>
 
       <div className="page-footer">
         {page.links?.length > 0 && (
